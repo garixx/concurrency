616ac1ba0ad37700522045026c940de2691e4859
garixx <akirraint@gmail.com>
2022-11-02 08:05:06 +0200

    reverted to initial state

--- 1/main.go
+++ 1/main.go
@@ -2,68 +2,9 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-// Option 1: using range in consumer
-func producerOption1(stream Stream) chan *Tweet {
-	tw := make(chan *Tweet)
-	go func() {
-		for {
-			tweet, err := stream.Next()
-			if err == ErrEOF {
-				close(tw)
-				return
-			}
-			tw <- tweet
-		}
-	}()
-	return tw
-}
-
-func consumerOption1(tw chan *Tweet) {
-	for tweet := range tw {
-		if tweet.IsTalkingAboutGo() {
-			fmt.Println(tweet.Username, "\ttweets about golang")
-			continue
-		}
-
-		fmt.Println(tweet.Username, "\tdoes not tweet about golang")
-	}
-}
-
-// Option 2: using select in consumer
-func producerOption2(done chan struct{}, stream Stream) chan *Tweet {
-	tw := make(chan *Tweet)
-	go func() {
-		for {
-			tweet, err := stream.Next()
-			if err == ErrEOF {
-				close(tw)
-				close(done)
-				return
-			}
-			tw <- tweet
-		}
-	}()
-	return tw
-}
-
-func consumerOption2(done chan struct{}, tw chan *Tweet) {
-	for {
-		select {
-		case tweet := <-tw:
-			if tweet.IsTalkingAboutGo() {
-				fmt.Println(tweet.Username, "\ttweets about golang")
-			} else {
-				fmt.Println(tweet.Username, "\tdoes not tweet about golang")
-			}
-		case done <- struct{}{}:
-			return
-		}
-	}
-}
-
-// Task
 func producer(stream Stream) (tweets []*Tweet) {
 	for {
 		tweet, err := stream.Next()
@@ -88,21 +29,15 @@ func consumer(tweets []*Tweet) {
 }
 
 func main() {
-	// TODO ... stub
-}
+	start := time.Now()
+	stream := GetMockStream()
 
-// Original main
-//
-//func main() {
-//	start := time.Now()
-//	stream := GetMockStream()
-//
-//	// Modification starts from here
-//	// Hint: this can be resolved via channels
-//	// Producer
-//	tweets := producer(stream)
-//	// Consumer
-//	consumer(tweets)
-//
-//	fmt.Printf("Process took %s\n", time.Since(start))
-//}
+	// Modification starts from here
+	// Hint: this can be resolved via channels
+	// Producer
+	tweets := producer(stream)
+	// Consumer
+	consumer(tweets)
+
+	fmt.Printf("Process took %s\n", time.Since(start))
+}
